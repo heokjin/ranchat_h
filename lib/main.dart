15da1f9b139aa0b7e6a4ec1e980d15da3984a482
YOON HYUKJIN <heokjin@gmail.com>
2019-06-09 22:47:17 +0900

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
+import 'package:ranchat_h/util/Themes.dart';
+
+import 'ChatScreen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Random Chat',
+      theme: defaultTargetPlatform == TargetPlatform.iOS
+          ? Themes.kIOSTheme
+          : Themes.kDefaultTheme,
+      home: new ChatScreen(),
+    );
+  }
+}
